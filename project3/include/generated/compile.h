4ff66e6417755868fe4daa950c8ed490af9653b8
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2014-11-09 20:56:38 -0800

    setup project3 folder more appropriately this time

---
+++ project3/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP */
+#define UTS_MACHINE "i386"
+#define UTS_VERSION "#1 SMP Thu Aug 9 14:37:39 PDT 2012"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "squidly001"
+#define LINUX_COMPILER "gcc version 4.5.3 (Gentoo 4.5.3-r2 p1.5, pie-0.4.7) "
