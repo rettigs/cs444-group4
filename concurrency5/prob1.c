e4b14f903ba562514b9c394c75921e990df18ae9
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2014-11-27 10:39:58 -0800

    problem 1 of Concurrency 5 assignment completed

--- concurrency5/prob1.c
+++ concurrency5/prob1.c
@@ -1,104 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>		// sleep()
 #include <pthread.h>
 #include <semaphore.h>
+#include <unistd.h>   // sleep()
 
-#define THREAD_MAX 30
+typedef struct _thread_data_t {
+  int tid;
+  int wait;
+} thread_data_t;
 
-sem_t resource;	// resource lock that allows only 3 threads to use the resource at any given time
-sem_t entry;	// restricts threads from attempting to obtain resource until all threads have exited
-pthread_barrier_t exit; // threshold for releasing entry lock to allow new threads to obtain the resource
+sem_t resource;
+pthread_barrier_t barrier;
+volatile int threads_active;
+volatile int set_lock;
+pthread_cond_t cond;
+pthread_mutex_t lock;
 
-void * thread_task();
+void * thread_task(void *arg);
 
 int main(int argc, char *argv[])
 {
-	pthread_t threads[10];
-	
-	pthread_barrier_init(&barrier, NULL, 3);
-	sem_init
+  srand(32);
+  sem_init(&resource, 0, 3);
+  pthread_barrier_init(&barrier, NULL, 3);
+  pthread_cond_init(&cond, NULL);
+  pthread_mutex_init(&lock, NULL);
+  threads_active = 0;
+  set_lock = 0;
 
-	if (argc != 2) {
-		printf("use: prob1 <num_threads>\n");
-		exit(-1);
-	}
+  int num_threads = 3;
+  pthread_t * threads;
+  thread_data_t * thread_dataset;
+  
+  if (argc >= 2) {
+    num_threads = atoi(argv[1]);
+  }
 
-	int num_threads = atoi(argv[1]);
+  printf("number of threads: %d\n\n", num_threads);
+  threads = malloc(num_threads * sizeof(pthread_t));
+  thread_dataset = malloc(num_threads * sizeof(thread_data_t));
 
-	if (num_threads > THREAD_MAX) {
-		printf("error: maximum number of threads allowed is %d.\n", THREAD_MAX);
-		exit(-1);
-	}
+  int i;
+  for (i = 0; i < num_threads; i++) {
+    thread_dataset[i].tid = i;
+    thread_dataset[i].wait = rand() % 10;
 
+    // lock until all threads have exited the resource
+    pthread_mutex_lock(&lock);
+    while(set_lock || threads_active >= 3) {
+      pthread_cond_wait(&cond, &lock);
+    }
+    pthread_mutex_unlock(&lock);
 
-	sem_init(&waiting_room, 0, num_chairs);
-	sem_init(&barber_chair, 0, 1);
-	sem_init(&in_progress, 0, 0);
-	sem_init(&nap_time, 0, 0);
+    int rc = pthread_create(&threads[i], NULL, thread_task, &thread_dataset[i]);
+    if (rc) {
+      fprintf(stderr, "error: pthread_create, return code : %d\n", rc);
+      exit(EXIT_FAILURE);
+    }
+  }
 
+  for (i = 0; i < num_threads; i++) {
+    pthread_join(threads[i], NULL);
+  }
 
-
-	for (t = 0; t < NUM_INSERTERS; t++) {
-		rc = pthread_create(&inserters[t], NULL, inserter, NULL);
-		if (rc) {
-			printf("ERROR: return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
-	}
-
-
-
-	
-
-	
-
-	for (i = 0; i < MAX_CUSTOMERS; i++) {
-		count[i] = i;
-	}
-
-	sem_init(&waiting_room, 0, num_chairs);
-	sem_init(&barber_chair, 0, 1);
-	sem_init(&in_progress, 0, 0);
-	sem_init(&nap_time, 0, 0);
-
-	
-
-	for (i = 0; i < num_customers; i++) {
-		pthread_create(&customer[i], NULL, customer_proc, (void *) &count[i]);
-	}
-
-	for (i = 0; i < num_customers; i++) {
-		pthread_join(customer[i], NULL);
-	}
-
-	end_of_day = 1; // all customers serviced, time to exit barber thread
-	sem_post(&nap_time);
-	pthread_join(barber, NULL);
-
-	exit(0);
+  sem_destroy(&resource);
+  free(thread_dataset);
+  free(threads);
+  exit(EXIT_SUCCESS);
 }
 
-void * customer_proc(void * number)
+void * thread_task(void *arg)
 {
-	int num = *(int *) number;
+  thread_data_t *data = (thread_data_t *)arg;
 
-	randwait(5);
-	printf("customer %d arrived at barber shop.\n", num);
+  printf("thread %d: running\n", data->tid);
 
-	get_hair_cut(num);
+  sem_wait(&resource);
+  printf("thread %d: obtained semaphore\n", data->tid);
 
-	return 0;
-}
+  pthread_mutex_lock(&lock);
+  threads_active++;
+  if (threads_active >= 3) {
+    set_lock = 1;
+  }
+  pthread_cond_signal(&cond);
+  pthread_mutex_unlock(&lock);
 
-void * barber_proc(void * none)
-{
-	while (!end_of_day) {
-		printf("barber is sleeping.\n");
-		sem_wait(&nap_time);
+  printf("thread %d: using resource\n", data->tid);
+  sleep(data->wait);
+  printf("thread %d: resource released\n", data->tid);
 
-		cut_hair();
-	}
+  pthread_barrier_wait(&barrier);
+  printf("thread %d: passed barrier\n", data->tid);
 
-	return 0;
+  pthread_mutex_lock(&lock);
+  threads_active--;
+  if (threads_active == 0) {
+    set_lock = 0;
+  }
+  pthread_cond_broadcast(&cond);
+  pthread_mutex_unlock(&lock);  
+
+  printf("thread %d: released semaphore\n", data->tid);
+  sem_post(&resource);
+
+  printf("thread %d: exiting\n", data->tid);
+  pthread_exit(NULL);
 }
